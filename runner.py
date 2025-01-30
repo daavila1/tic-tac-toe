455dbfedef879266ac77cd7d4cc8e74cc680f139
daniel <daavilaga@unal.edu.co>
2025-01-29 19:10:09 -0500

    Hello, pygame! ran successfully

--- runner.py
+++ runner.py
@@ -8,17 +8,31 @@ import logic
 pygame.init()
 size = width, height = 600, 600 # Window size
 
+# Set up the display
 screen = pygame.display.set_mode(size)
 
-# Color
-black = (0, 0, 0)
+# Load system font
+font = pygame.font.SysFont('Arial', 30)
 
-while True:
-    
-    # Get the event context. If event quit: close window
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+
+# Main loop
+running = True
+while running:    
+
     for event in pygame.event.get():
+        # If user quit: stop running loop
         if event.type == pygame.QUIT:
-            sys.exit()
-            
+            running = False
+    
     screen.fill(black)
     
+    title = font.render("Hello, pygame!", True, white)
+    title_rect = title.get_rect()
+    title_rect.center = ((width / 2), 100)
+    screen.blit(title, title_rect)
+    
+    pygame.display.flip()
